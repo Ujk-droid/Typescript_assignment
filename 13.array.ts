4648d06542dfbf7bc11900fa1f83a580fb3e35b4
Ujk-droid <155759233+Ujk-droid@users.noreply.github.com>
2024-02-27 07:51:22 -0800

    Add files via upload

---
+++ 13.array.ts
@@ -0,0 +1,9 @@
+
+let transportation :string[] = [' motercycle','car','train','bus'];
+
+
+for(let i=0; i<transportation.length; i++){
+
+    console.log('I would like to own a' + transportation[i])
+
+}

b43a6141139d8c42721264e5f6f15c38a0486502
Ujk-droid <155759233+Ujk-droid@users.noreply.github.com>
2024-02-27 07:50:00 -0800

    Add files via upload

---
+++ 12.greeting.ts
@@ -0,0 +1,11 @@
+
+let names :string[] = ['uzma','muddassir','malahim','hadi'];
+
+const message :string = 'welcome to type script class: '
+ 
+
+for (let i=0; i<names.length; i++){
+
+    console.log(message + names[i]) ;
+    
+}

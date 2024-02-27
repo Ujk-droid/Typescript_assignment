63e29caa6725db590f3da10155762a82ab061984
Ujk-droid <155759233+Ujk-droid@users.noreply.github.com>
2024-02-27 08:05:08 -0800

    Add files via upload

---
+++ 19.inviting_to_dinner.ts
@@ -0,0 +1,7 @@
+
+
+let Guest_list :string[] = ['Imrankham','Malahim','Kamrankhan','Sirziakhan','Arib','Hammad'];
+
+for(let i = 0; i < Guest_list.length; i++){
+    console.log('Dear Mr.' + Guest_list[i] + ',\n\nIt is our pleasure to invite you to dinner on Sunday 9:30 pm.\n\nThankyou!\n\n')
+}

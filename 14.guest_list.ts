898ff4394ed68085ad451198f2cd3da8fee1a1a9
Ujk-droid <155759233+Ujk-droid@users.noreply.github.com>
2024-02-27 07:52:39 -0800

    Add files via upload

---
+++ 14.guest_list.ts
@@ -0,0 +1,8 @@
+
+let Guest_list :string[] = ['UZMAKHAN','MALAHIM','HADI','RIDA'];
+
+for(let i = 0; i < Guest_list.length; i++){
+console.log('Dear Mr. ' + Guest_list[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank you!')
+
+
+}

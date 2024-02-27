7fdc9b5c57149fae10d62703e21f619a165e078a
Ujk-droid <155759233+Ujk-droid@users.noreply.github.com>
2024-02-27 07:44:51 -0800

    Add files via upload

---
+++ 05.famouse.ts
@@ -0,0 +1,6 @@
+
+let famouse_person :string = "Imran Khan";
+
+let message :string = "sab sy pahly to ap ny ghabrana nahi hy Allah sy madad mangni hy";
+
+console.log(`${famouse_person}, Once said, ${message}`);

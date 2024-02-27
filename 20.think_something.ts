74475e4012763285c6fd7391a33950067d6144d1
Ujk-droid <155759233+Ujk-droid@users.noreply.github.com>
2024-02-27 08:07:04 -0800

    Add files via upload

---
+++ 20.think_something.ts
@@ -0,0 +1,8 @@
+// Create an array of countries
+const countries: string[] = ["USA", "Canada", "Australia", "Japan", "Brazil"];
+
+// Print the list of countries
+console.log("List of countries:");
+countries.forEach(country => {
+    console.log(country);
+});

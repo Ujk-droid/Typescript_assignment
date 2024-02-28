27cd07d9eecfd449d79926bf9160d42ec3f2ce2c
Ujk-droid <155759233+Ujk-droid@users.noreply.github.com>
2024-02-28 06:59:37 -0800

    Add files via upload

---
+++ 36.shirt.ts
@@ -0,0 +1,7 @@
+
+function make_shirt(size: string, message: string): void {
+    console.log(`The shirt size is ${size.toUpperCase()} and it will have the message: "${message}".`);
+}
+
+// Calling the function
+make_shirt('medium', 'Hello, World!');

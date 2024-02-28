52eb16785e0a98c4ce43a6943b8f72b2b91a8946
Ujk-droid <155759233+Ujk-droid@users.noreply.github.com>
2024-02-28 07:06:59 -0800

    Delete 37.cities.ts

--- 37.cities.ts
+++
@@ -1,9 +0,0 @@
-
-function describe_city(city: string, country: string = "Pakistan"): void {
-    console.log(`${city} is in ${country}.`);
-}
-
-// Calling the function for three different cities
-describe_city("Karachi");
-describe_city("Paris", "France");
-describe_city("New York", "USA");

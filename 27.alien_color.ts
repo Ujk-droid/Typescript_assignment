7d28828890b84b993874183c461ec9694bdf569a
Ujk-droid <155759233+Ujk-droid@users.noreply.github.com>
2024-02-27 21:43:42 -0800

    Add files via upload

---
+++ 27.alien_color.ts
@@ -0,0 +1,10 @@
+
+let alien_color: string = 'green';
+
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+} else if (alien_color === 'yellow') {
+    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
+} else if (alien_color === 'red') {
+    console.log("Congratulations! You just earned 15 points for shooting the red alien.");
+}
